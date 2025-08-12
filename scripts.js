35e0208557aae6061cf14f8e030c0873c47ec834
Andrey Richardson <andreyrichardson45@gmail.com>
2025-08-12 14:30:02 -0300

    add assinatura

--- scripts.js
+++ scripts.js
@@ -20,3 +20,28 @@ document.addEventListener('DOMContentLoaded', function(){
         });
     });
 });
+// Obtém os elementos checkbox do HTML pelo ID
+const checkbox1 = document.getElementById('checkbox1');
+const checkbox2 = document.getElementById('checkbox2');
+
+// Adiciona um "listener" para o evento 'change' do primeiro checkbox
+checkbox1.addEventListener('change', function() {
+  if (this.checked) {
+    // Se o checkbox1 estiver marcado, desabilita o checkbox2
+    checkbox2.disabled = true;
+  } else {
+    // Se o checkbox1 for desmarcado, habilita o checkbox2
+    checkbox2.disabled = false;
+  }
+});
+
+// Adiciona um "listener" para o evento 'change' do segundo checkbox
+checkbox2.addEventListener('change', function() {
+  if (this.checked) {
+    // Se o checkbox2 estiver marcado, desabilita o checkbox1
+    checkbox1.disabled = true;
+  } else {
+    // Se o checkbox2 for desmarcado, habilita o checkbox1
+    checkbox1.disabled = false;
+  }
+});
